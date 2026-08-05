514b66064f6ed415f39f3b407a4786e7cec723c4
Angela Maria Lourenco <ANLOURENCO@firjan.com.br>
2026-08-05 12:29:59 -0300

    Renova cache do aplicativo publicado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'estudos-3-ano-v5';
+const CACHE = 'estudos-3-ano-v6';
 const ARQUIVOS = [
   './', './index.html', './style.css', './data.js', './experiences.js',
   './pedagogy.js', './app.js', './manifest.webmanifest',
