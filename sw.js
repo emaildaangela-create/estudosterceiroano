324b6e79da1824c09ea79f8f2feeb0dafa60c07b
Angela Maria Lourenco <ANLOURENCO@firjan.com.br>
2026-08-07 14:40:41 -0300

    Ajusta jogos para telas de celular

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'estudos-3-ano-v23';
+const CACHE = 'estudos-3-ano-v24';
 const ARQUIVOS = [
   './', './index.html', './style.css', './data.js', './experiences.js',
   './pedagogy.js', './app.js', './manifest.webmanifest',
